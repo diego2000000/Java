566f588d5b09c389d12adabd8e0ba1a8495bbbd5
diego2000000 <diego.rombaldoni@gmail.com>
2025-11-05 12:39:06 +0100

    finished menu and first function started on second function

--- Loop_User_Input/src/Main.java
+++ Loop_User_Input/src/Main.java
@@ -1,16 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner userInput=new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         String userName;
         byte option0to9;
         System.out.println("Hello and welcome ! May i know your name?");
         System.out.println("Your name:");
-        userName=userInput.nextLine();
-        System.out.println("Welcome "+userName);
-        while (true){
-            String [] menu = {
+        userName = userInput.nextLine();
+        System.out.println("Welcome " + userName);
+
+        mainLoop:
+// gives the overarching loop an id to directly break out of it
+        while (true) {
+            String[] menu = {
                     "(0) : Go back to startmenu",
                     "(1) : Sum from n to m (Example: 0-100 = 5050)",
                     "(2) : Guess my random number from n-m",
@@ -18,9 +22,80 @@ public class Main {
                     "(4) : Pastoral care",
                     "(5) : Temperature converter",
                     "(6) : Factor finder",
-                    "(7) : "
+                    "(7) : Sum of every nth number up to 1000",
+                    "(8) :",
+                    "(9) :"
             };
-            System.out.println();
+
+            for (String option : menu) {//String option assigns the individual array items to a string
+                System.out.println(option);
+            }
+            System.out.println("Which action do you want to perform:");
+            option0to9 = userInput.nextByte();
+
+            switch (option0to9) {
+                case 0:// exit function
+                    System.out.println("Do you want to continue the program? (y/n)");
+                    String yOrN;
+                    yOrN = userInput.nextLine();
+                    yOrN = userInput.nextLine();
+                    if (yOrN.equalsIgnoreCase("n")) {
+                        System.out.println("Goodbye " + userName);
+                        break mainLoop;
+                    }
+                    break;
+
+                case 1:
+                    yOrN="y";
+                    while (yOrN.equalsIgnoreCase("y")) {
+                        int number01;
+                        int number02;
+                        System.out.println("What is your start range? (integer)");
+                        number01 = userInput.nextInt();
+                        System.out.println("What is your end range? (integer)");
+                        number02 = userInput.nextInt();
+                        int result = number02 * (number02 + 1) / 2 - number01 * (number01 + 1) / 2 + number01;
+                        System.out.println(result);
+                        System.out.println("Would you like to rerun the function? (y/n)");
+                        yOrN=userInput.nextLine();
+                        yOrN=userInput.nextLine();
+                        if (yOrN.equalsIgnoreCase("n")) {
+                            break ;
+                        }
+                    }
+
+                case 2:
+                    yOrN="y";
+                    while (yOrN.equalsIgnoreCase("y")) {
+
+                        break;
+                    }
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                default:
+                    break;
+            }
+
         }
+
     }
 }
