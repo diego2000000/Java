f9e67d557d269bf2684fc9691ef9147fa9d30778
diego2000000 <diego.rombaldoni@gmail.com>
2025-11-05 22:52:54 +0100

    made player move and controllable

--- custum_Factorio_game/src/GamePanel.java
+++ custum_Factorio_game/src/GamePanel.java
@@ -11,13 +11,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol;//768 px
     final int screenHeight = tileSize * maxScreenRow;// 567 px
 
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //Set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));//sets window size
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);// should improve rendering performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);//focus on key input
     }
 
     public void startGameThread() {
@@ -29,14 +40,61 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1_000_000_000 / FPS;//0.16666666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;//System.nanoTime is the current in game Time
+
         while (gameThread != null) {
 
-            //System.out.println("The game loop is running");
-
             // 1 UPDATE: update info such as charakter positions
+            update();
 
             // 2 DRAW: draw the screen with the updated info
+            repaint();
+
+            try {
+
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1_000_000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
 
+    }
+
+    public void update() {
+
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);//super calls JComponent
+
+        Graphics2D g2 = (Graphics2D) g;//Graphics2D has is mor controllable
+
+        g2.setColor(Color.white);//specifies color for g2
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);//draws a rectangle in specified color
+
+        g2.dispose();
     }
 }
