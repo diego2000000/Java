f3b25231e6a24c4f9bd2eef02a11715867795619
diego2000000 <diego.rombaldoni@gmail.com>
2025-11-12 11:15:57 +0100

    completed exercise 12

--- 12_A_Kontrollstrukturen-Schleifen-For/src/MainY.java
+++ 12_A_Kontrollstrukturen-Schleifen-For/src/MainY.java
@@ -16,11 +16,11 @@ public class MainY {
         //      }
         //
         //      Questions to consider:
-        //      1. What would happen if you ran this code as is?
-        //      2. Why does this behavior occur?
+        //      1. What would happen if you ran this code as is? code would run forever
+        //      2. Why does this behavior occur? because iteratorEx1 is always lower than 10
         //      3. How can you modify the code to achieve the desired output:
         //         i:0 i:1 i:2 i:3 i:4 i:5 i:6 i:7 i:8 i:9
-
+        //        for (int iteratorEx1 = 0; iteratorEx1 < 10; iteratorEx1++)
 
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 02");
@@ -35,7 +35,85 @@ public class MainY {
         //      02-08. 15 to -20 (descending)
         //      02-09. Even numbers from 10 to -10 (descending)
         //      02-10. Odd numbers from 10 to -10 (descending)
+        System.out.println("02-01");
+        for (int number = 0; number <= 100; number++) {
 
+            System.out.print(number);
+        }
+        System.out.println();
+
+        System.out.println("02-02");
+        for (int number = 42; number <= 100; number++) {
+
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("02-03");
+        for (int number = 42; number <= 123; number++) {
+
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("02-04");
+        for (int number = 0; number <= 10; number++) {
+            if ((number % 2) == 0) {
+
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("02-05");
+        for (int number = 0; number <= 10; number++) {
+            if ((number % 2) != 0) {
+
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("02-06");
+        for (int number = -10; number <= 20; number++) {
+
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("02-07");
+        for (int number = 35; number >= 0; number--) {
+
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("02-08");
+        for (int number = 15; number >= -20; number--) {
+
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        System.out.println("02-09");
+        for (int number = 10; number >= -10; number--) {
+
+            if ((number % 2) == 0) {
+
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("02-10");
+        for (int number = 10; number >= -10; number--) {
+
+            if ((number % 2) != 0) {
+
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
 
         //--------------------------------------------------------------------------------------------------------------
         System.out.println("Exercise 03");
@@ -44,7 +122,9 @@ public class MainY {
         int forSum_03 = 0;
 
         // Your code here
-
+        for (int number = 0; number <= 100; number++) {
+            forSum_03 = forSum_03 + number;
+        }
         System.out.println(forSum_03); // Should be 5050
 
         //--------------------------------------------------------------------------------------------------------------
@@ -54,6 +134,14 @@ public class MainY {
         int forSum_04 = 0;
 
         // Your code here
+        for (int number = 0; number <= 100; number++) {
+
+            if ((number%2)==0){
+
+                forSum_04 = forSum_04 + number;
+            }
+
+        }
 
         System.out.println(forSum_04); // Should be 2550
 
@@ -64,7 +152,14 @@ public class MainY {
         int forSum_05 = 0;
 
         // Your code here
+        for (int number = 0; number <= 100; number++) {
 
+            if ((number%2)!=0){
+
+                forSum_05 = forSum_05 + number;
+            }
+
+        }
         System.out.println(forSum_05); // Should be 2500
 
     }
